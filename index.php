750e8873d87f5629705c01684a1f5f0a0c5d5e16
albertaw <alberta04@gmail.com>
2013-10-07 12:26:48 -0500

    add header to index

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php include 'header.php'; ?>		
-
+<doctype! html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css" >
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.js"></script>
+	</head>
+	<body id="games">
+	
+		<div id="navbar">
+			<ul class="">
+				<li><a href="index.php"><img src="img/icons/home-32.png"></a></li>
+				<li><a href="games.php"><img src="img/icons/dice-32.png"></a></li>
+			
+				<li><a href="https://github.com/albertaw"><img src="img/icons/github-32.png"></a></li>
+				<li><a href="https://twitter.com/artificialAl"><img src="img/icons/twitter-32.png"></a></li>
+				<li><a href="mailto:alberta04@gmail.com" target="_top"><img src="img/icons/email-32.png"></a></li>
+	
+			</ul>
+		</div><!--------------end nav------------------------->
+	
 	<div class="main container">
 		<div class="row">
 		<h1 class="message" class="span12">Let's Play Bingo!</h1>
