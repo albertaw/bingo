011334363f065e289cff39ade0074f2f2ea458c8
Alberta Williams <alberta@cerebromusic.com>
2015-08-31 23:57:28 -0500

    changes port to environment variable

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ var express = require('express');
 var serveStatic = require('serve-static');
 var serveIndex = require('serve-index');
 
-var app = express()
-	.use(express.static(__dirname + '/public'))
-	.use(serveIndex(__dirname + '/public'))
-	.listen(3000);
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+app.use(serveIndex(__dirname + '/public'))
+
+app.listen(app.get('port'), function () {
+	console.log('app running on port', app.get('port'));
+});
