82328e6a74448b4eec8db50e277972e0882c68f5
Alberta Williams <alberta@cerebromusic.com>
2015-09-01 01:09:34 -0500

    removes unecessary node modules

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var express = require('express');
-var serveStatic = require('serve-static');
-var serveIndex = require('serve-index');
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-app.use(serveIndex(__dirname + '/public'))
 
 app.listen(app.get('port'), function () {
 	console.log('app running on port', app.get('port'));
